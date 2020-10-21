cb612ec1e9d7aa2d499de5ab52104d8fc69dabf8
achraf <achrafatauil@hotmail.com>
2020-10-21 16:09:48 +0200

    Make increment & decrement functional + key input validation

--- addon/components/au-time-picker.js
+++ addon/components/au-time-picker.js
@@ -3,5 +3,61 @@ import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class AuTimePickerComponent extends Component {
+  @tracked hourValue = 12;
+  @tracked minuteValue = 0;
+  @tracked secondValue = 0;
 
+  @tracked keyCodes = [ 8 , 13 , 33, 32 , 34 , 38, 40, 46 ]
+
+  @action increment(elem){ 
+
+    if(elem == "hourValue"){
+      this[elem]++
+
+      if(this[elem] >= 24){
+        this[elem] = 24
+      }
+    }
+
+    if(elem != "hourValue"){
+      this[elem]++
+
+      if(this[elem] >= 60){
+        this[elem] = 60
+      }
+    }
+  }
+
+  @action decrement(elem){
+
+    if(elem == "hourValue"){
+      this[elem]--
+
+      if(this[elem] <= 1){
+        this[elem] = 1
+      }
+    }
+
+    if(elem != "hourValue"){
+      this[elem]--
+
+      if(this[elem] <= 0){
+        this[elem] = 0
+      }
+    }
+  }
+
+  // Keyboard input
+
+  @action setTimeValue(e){
+    console.log(isNaN(e.key))
+    console.log(isNaN(e.key) && this.keyCodes.indexOf(e.keyCode) == -1 )
+    if(isNaN(e.key) && this.keyCodes.indexOf(e.keyCode) == -1 ){
+      console.log(e)
+      e.preventDefault();
+    } else {
+      
+      this.hourValue = e.target.value
+    }
+  }
 };
